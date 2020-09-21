4b45ede61b6cac172ebe1528971174c78b466cfc
Lefebvre, Robert A <lefebvrera@ornl.gov>
2020-09-21 15:09:04 -0400

    Remove compiler directive from test config script

--- ci/linux_gcc_test.sh
+++ ci/linux_gcc_test.sh
@@ -4,13 +4,11 @@ cd build
 
 module load toolchain/gcc/4.8.3 cmake-3.13.2
 
-cmake -DBUILDNAME="$(uname -s)-GCC-4.8.3-Release-${CI_BUILD_REF_NAME}" \
+cmake -DBUILDNAME="$(uname -s)-Release-${CI_BUILD_REF_NAME}" \
       -DCMAKE_BUILD_TYPE=RELEASE \
       -Dsaline_ENABLE_TESTS=ON \
       -DBUILD_SHARED_LIBS:BOOL=ON \
       -Dsaline_ENABLE_ALL_PACKAGES=ON \
-      -DCMAKE_CXX_COMPILER:FILEPATH=`which g++` \
-      -DCMAKE_C_COMPILER:FILEPATH=`which gcc` \
       ..
 
 ctest --output-on-failure \
