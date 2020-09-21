cb05ff1b708423b0b429c8d69824a904a4e0599d
Lefebvre, Robert A <lefebvrera@ornl.gov>
2020-09-21 14:45:07 -0400

    Add initial CI for test, memory and coverage

---
+++ ci/linux_gcc_analysis.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -ex
+mkdir build
+cd build
+module load toolchain/gcc/4.8.3 cmake valgrind
+
+cmake -MEMCHECK_COMMAND="$(which valgrind)" \
+      -DBUILDNAME="$(uname -s)-GCC-4.8.3-Debug-${CI_BUILD_REF_NAME}" \
+      -DCMAKE_BUILD_TYPE=DEBUG \
+      -Dsaline_ENABLE_COVERAGE_TESTING=ON \
+      -DCOVERAGE_EXTRA_FLAGS="-s ${CI_PROJECT_DIR}/googletest -d" \
+      -Dsaline_ENABLE_TESTS=ON \
+      -Dsaline_ENABLE_ALL_PACKAGES=ON \
+      ..
+
+ctest --output-on-failure \
+      -D Experimental -j 8 \
+      -D ExperimentalMemCheck \
+      -D ExperimentalCodeCoverage \
+      -D ExperimentalSubmit
