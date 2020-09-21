f849febcd345135040e84128ae60714e4d0d8f89
Lefebvre, Robert A <lefebvrera@ornl.gov>
2020-09-21 15:44:39 -0400

    Update Fortran with latest expected viscosity (mu) test value for known mole distribution

--- fortran/tests/tstthermophysical_properties.f90
+++ fortran/tests/tstthermophysical_properties.f90
@@ -20,16 +20,15 @@ call tp%initialize()
 ! Assign data store to
 call tp%init_data_store(data)
 
-if (.not.tp%set_composition("LiF-BeF2-ThF4",[0.6998_8,0.1499_8,0.1503_8],3)) then
+if (.not.tp%set_composition("LiF-BeF2-ThF4",[0.7011_8, 0.2388_8, 0.601_8],3)) then
   print *,"Failure to set composition!"
   stop 1
 endif
 
-print *,tp%mu(823.15_8, 101.0_8)
-if ( tp%mu(823.15_8, 101.0_8) < 13.5_8 .or. 13.6_8 < tp%mu(823.15_8, 101.0_8)) then
-
-print *,"Failed to equate values!"
-stop 2
+print *,tp%mu(926.00_8, 101.0_8)
+if ( tp%mu(926.00_8, 101.0_8) /= 7.4799345781764925_8) then
+  print *,"Failed to equate values!"
+  stop 2
 endif
 
 call tp%destroy()
