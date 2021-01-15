a0263fe4a4d289886640b35772feac908214aa8e
Shane C. Henderson <hendersonsc@ornl.gov>
2021-01-15 14:44:29 -0500

    Remove change to cmake for fortan in wsl

--- cmake/CallbackSetupExtraOptions.cmake
+++ cmake/CallbackSetupExtraOptions.cmake
@@ -61,6 +61,12 @@ MACRO(TRIBITS_REPOSITORY_SETUP_EXTRA_OPTIONS)
   #
   # Set up saline cmake directory, used by default option scripts
   SET(saline_CMAKE_DIR "${saline_SOURCE_DIR}/cmake" CACHE PATH "")
+  IF(NOT "$ENV{${PROJECT_NAME}_ENABLE_Fortran}" STREQUAL "" )
+     SET(${PROJECT_NAME}_ENABLE_Fortran $ENV{${PROJECT_NAME}_ENABLE_Fortran})
+  ELSE()
+     # ensure fortran compiler is set on
+     SET(${PROJECT_NAME}_ENABLE_Fortran OFF)
+  ENDIF()
 
 
   ########################################
